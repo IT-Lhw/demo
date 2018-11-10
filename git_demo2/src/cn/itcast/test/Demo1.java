6790a0c5b2cf6d79f18e040f71df89bc436fb64f
lihongwei <1747908055@qq.com>
2018-11-10 19:55:16 +0800

    修改demo1

---
+++ git_demo2/src/cn/itcast/test/Demo1.java
@@ -0,0 +1,9 @@
+package cn.itcast.test;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+        System.out.println("1111111");
+        System.out.println(11111);
+    }
+}
